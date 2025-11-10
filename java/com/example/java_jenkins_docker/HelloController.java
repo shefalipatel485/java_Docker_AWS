0de9657b009c5ca1dde2bf2f92c73e3491c3221e
shefali patel <shefalip2206@gmail.com>
2025-11-10 15:17:59 +0530

    Add files via upload

---
+++ java/com/example/java_jenkins_docker/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.java_jenkins_docker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+	@GetMapping("/")
+    public String home() {
+        return "✅ Hello from AWS EC2 Java App!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is running fine!";
+    }
+}
---
+++ java/com/example/java_jenkins_docker/JavaJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_jenkins_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJenkinsDockerApplication.class, args);
+	}
+
+}
